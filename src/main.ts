d69022c3610ea0afdbb8718c28291c0ea55bea73
aceaspades <aceaspades@protonmail.com>
2025-07-07 19:59:09 -0700

    Added crude unhandled rejection + unhandled exception handling

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import Queues from './modules/queues'
 import WebhookServer from './modules/webhooks'
 import Subscriber from './modules/subscriber'
 
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason)
+})
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err)
+})
+
 try {
   Settings.init()
   Queues.init()
